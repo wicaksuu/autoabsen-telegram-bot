819d491ad9085907ac6a750164e172b699272e14
Wicaksu <wicaksu@wicaksu.com>
2023-12-14 19:57:35 +0700

    d

--- app/Http/Controllers/Absen/TelegramBalsanController.php
+++ app/Http/Controllers/Absen/TelegramBalsanController.php
@@ -103,7 +103,7 @@ class TelegramBalsanController extends Controller
                         $data_absen->aksi_terakhir = 'selesaiInput';
                         $data_absen->password = $messageText;
                         $data_absen->save();
-                        Telegram::sendMessage(['chat_id' => $chatId, 'text' => "Hai " . $data_absen->nama . "($messageText)\n\nSilahkan Masukkan Password Akun Presensi Anda !"]);
+                        Telegram::sendMessage(['chat_id' => $chatId, 'text' => "Hai " . $data_absen->nama . "(" . $data_absen->nip . ")\n\nSilahkan Masukkan Password Akun Presensi Anda !"]);
                         break;
                     case 'selesaiInput':
                         $data_absen->aksi_terakhir = 'cekAkun';
